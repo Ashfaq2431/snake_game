b7b4d5906ce668726ad5bf72ceb14dfe8db83baf
Ashfaq2431 <ashfaqjain2402@gmail.com>
2026-01-01 18:52:02 +0530

    Refactor Snake class and add reset method
    Refactor Snake class to improve structure and add reset functionality.

--- snake.py
+++ snake.py
@@ -1,39 +1,56 @@
+XPOS = [(0, 0), (-20, 0), (-40, 0)]
+MOVE_DISTANCE = 20
 from turtle import Turtle
-STARTING_POSITION=[(0,0),(-20,0),(-40,0)]
-MOVE_DISTANCE=20
-DIRECTION=[0,90,180,270]
+
+
 class Snake:
     def __init__(self):
-        self.segments=[]
+        self.segments = []
         self.create_snake()
+        self.head = self.segments[0]
+
     def create_snake(self):
-        for i in STARTING_POSITION:
+        for i in XPOS:
             self.add_segment(i)
-    def add_segment(self,i):
-        new_segment = Turtle("square")
-        new_segment.color("white")
-        new_segment.penup()
-        new_segment.goto(i)
-        self.segments.append(new_segment)
+
+    def add_segment(self,position):
+        t = Turtle('square')
+        t.color('green')
+        t.penup()
+        t.goto(position)
+        self.segments.append(t)
 
     def extend(self):
         self.add_segment(self.segments[-1].position())
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000,1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
+
+
     def move(self):
-        for seg_num in range(len(self.segments) - 1, 0, -1):
-            new_x = self.segments[seg_num - 1].xcor()
-            new_y = self.segments[seg_num - 1].ycor()
-            self.segments[seg_num].goto(new_x, new_y)
-        self.segments[0].forward(MOVE_DISTANCE)
+        for seg in range(len(self.segments) - 1, 0, -1):
+            x = self.segments[seg - 1].xcor()
+            y = self.segments[seg - 1].ycor()
+            self.segments[seg].goto(x, y)
+        self.head.forward(MOVE_DISTANCE)
+
     def up(self):
-        if self.segments[0].heading() != DIRECTION[3]:
-           self.segments[0].seth(90)
+        if self.head.heading() != 270:
+            self.head.setheading(90)
+
     def down(self):
-        if self.segments[0].heading() != DIRECTION[1]:
-           self.segments[0].seth(270)
+        if self.head.heading() != 90:
+            self.head.setheading(270)
+
     def left(self):
-        if self.segments[0].heading() != DIRECTION[0]:
-           self.segments[0].seth(180)
+        if self.head.heading() != 0:
+            self.head.setheading(180)
+
     def right(self):
-        if self.segments[0].heading() != DIRECTION[2]:
-           self.segments[0].seth(0)
+        if self.head.heading() != 180:
+            self.head.setheading(0)
 
